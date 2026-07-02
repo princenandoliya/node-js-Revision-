794825ca6d96c0655e06b4ee93e3dbc3fdcba728
prince <princenandoliya@gmail.com>
2026-07-02 12:03:13 +0530

    new code add

---
+++ 11_O_auth/middleware/HttpError.js
@@ -0,0 +1,9 @@
+class HttpError extends Error{
+    constructor(message,StatusCode){
+        super(message)
+
+        this.StatusCode = StatusCode
+    }
+}
+
+export default HttpError
---
+++ 11_O_auth/server.js
@@ -0,0 +1,35 @@
+import express from "express"
+import HttpError from "./middleware/HttpError.js"
+
+
+const app = express()
+
+
+app.get("/",(req,res)=>{
+    res.send("hello from server")
+})
+
+app.use((req,res,next)=>{
+    next(new HttpError("requested routes are not found"))
+})
+
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    res.status(err.StatusCode || 500).json({
+        success: false,
+        message: err.message
+    });
+});
+
+
+const port = 5000
+
+app.listen(port,(err)=>{
+    if(err){
+        return console.log(err.message)
+    }
+
+    console.log(`server runing on port ${port}`)
+})
+
