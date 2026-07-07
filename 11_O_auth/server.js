57afcf38db36a673676da7404adbedf4f1f496d8
prince <princenandoliya@gmail.com>
2026-07-07 11:27:27 +0530

    new code add

--- 11_O_auth/server.js
+++ 11_O_auth/server.js
@@ -1,5 +1,8 @@
 import express from "express"
 import HttpError from "./middleware/HttpError.js"
+import connectDB from "./config/db.js"
+import dotenv from "dotenv"
+dotenv.config({path: "./.env"})
 
 
 const app = express()
@@ -25,11 +28,28 @@ app.use((err, req, res, next) => {
 
 const port = 5000
 
-app.listen(port,(err)=>{
-    if(err){
-        return console.log(err.message)
+async function server() {
+    try {
+
+        const connect = await connectDB()
+
+        if (!connect) {
+            throw new error("fail to connect DB")
+        }
+        app.listen(port, (err) => {
+            if (err) {
+                return console.log(err.message)
+            }
+
+            console.log(`server runing on port ${port}`)
+        })
+
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+
     }
 
-    console.log(`server runing on port ${port}`)
-})
+}
+server()
 
